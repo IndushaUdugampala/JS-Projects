6f7872db95a95d92f713a04e54a977068be60d70
Indusha <indushaudugampala@gmail.com>
2023-08-17 10:07:59 +0530

    Meal Maker project created.

---
+++ Meal Maker/app.js
@@ -0,0 +1,25 @@
+const menu = {
+    _meal: "",
+    _price: 0,
+    set meal(mealToCheck) {
+        if (typeof mealToCheck === "string") {
+            return _meal = mealToCheck;
+        }
+    },
+    set price(priceToCheck) {
+        if (typeof priceToCheck === "number") {
+            return _price = priceToCheck;
+        }
+    },
+    get todaysSpecial() {
+        if (_meal && _price) {
+            return 'Today’s Special is ' + _meal + ' for $' + _price + '!';
+        } else {
+            return 'Meal or price was not set correctly!';
+        }
+    }
+};
+
+menu.meal = "pizza";
+menu.price = 29;
+console.log(menu.todaysSpecial);
